19e3ef8b511275c03bcd558e02bb433d14bd236f
Zaid1477 <hammadling122@gmail.com>
2026-03-03 21:01:40 +0500

    Add files via upload

---
+++ Lab 1/Lab work/lab 1 labwork 1.cpp
@@ -0,0 +1,18 @@
+
+// Name: zaid rauf
+// Department: Electrical Engineering (Power)
+// Registration No: BF25NWELE0740
+// Section: A
+
+#include <iostream>   // Header file for input and output
+using namespace std;  // Using standard namespace
+
+int main() 
+{
+    // Printing Hello World on the screen
+    cout << "Hello World!";
+
+    return 0;  // Program ends here
+}
+
+
