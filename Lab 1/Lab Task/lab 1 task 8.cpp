d2b46bc18e87016ad2ee2d55ed3e0bedafeccb3e
Zaid1477 <hammadling122@gmail.com>
2026-03-03 17:49:15 +0500

    Add files via upload

---
+++ Lab 1/Lab Task/lab 1 task 8.cpp
@@ -0,0 +1,17 @@
+
+// Name: zaid rauf
+// Department: Electrical Engineering (Power)
+// Registration No: BF25NWELE0740
+// Section: A
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    float aB = 10.2;  // Declaring float variable
+
+    cout << aB;  // Printing value of aB
+
+    return 0;  // Program ends successfully
+}
