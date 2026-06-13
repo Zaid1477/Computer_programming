0f0acc0697170edbb5dc5d47ae4a2f36ccb6c1c0
Zaid1477 <hammadling122@gmail.com>
2026-06-13 12:11:38 +0500

    Add files via upload

---
+++ Lab11/Lab Work/Lab 11 LabWork 2.cpp
@@ -0,0 +1,20 @@
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    // Declaring and initializing first name
+    string firstName = "Zaid ";
+
+    // Declaring and initializing last name
+    string lastName = "Rauf";
+
+    // Concatenating first name and last name with a space in between
+    string fullName = firstName + " " + lastName;
+
+    // Displaying the full name
+    cout << "Full Name: " << fullName << endl;
+
+    return 0;
+}
