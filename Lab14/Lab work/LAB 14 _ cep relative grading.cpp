6fad6ad5a8db2a16aea703e7bbbf379d61c28286
Zaid1477 <hammadling122@gmail.com>
2026-06-26 19:41:34 +0500

    Update LAB 14 _ cep relative grading.cpp

--- Lab14/Lab work/LAB 14 _ cep relative grading.cpp
+++ Lab14/Lab work/LAB 14 _ cep relative grading.cpp
@@ -1,6 +1,6 @@
 // Name: ZAID RAUF
 // CEP: Relative Grading System
-// Student: Talha Ali | Reg: BF25NWELE0740
+// Student: Zaid Ruf | Reg: BF25NWELE0740
 // Semester 2 | UET Peshawar, Nowshera Campus
 // =====================================================
 
